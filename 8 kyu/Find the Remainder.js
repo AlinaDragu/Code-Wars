677bfd089fc975715223c6a3bc3565e99381bfc8
AlinaDragu <alina_elena14@yahoo.com>
2023-07-09 23:15:35 +0300

    8 kyu

--- 8 kyu/Find the Remainder.js
+++ 8 kyu/Find the Remainder.js
@@ -4,12 +4,9 @@
 
 function remainder(n, m){
     // Divide the larger argument by the smaller argument and return the remainder
-     if (n === 0 || m === 0) {
-      return NaN
-    }
-    
-    const larger = Math.max(n, m);
-    const smaller = Math.min(n, m);
-    
-    return larger % smaller
+     if(m>n){
+       return m%n
+     }else{
+      return n%m
+     }
   }
