df71409a9f2d3e5d9ab16332999e0642484dc138
AlinaDragu <alina_elena14@yahoo.com>
2024-01-16 20:05:43 +0200

    DOM

--- DOM/Etch-A-Sketch/main.js
+++ DOM/Etch-A-Sketch/main.js
@@ -1,24 +1,32 @@
- const container = document.getElementById('container');
+const container = document.getElementById('container');
 
-    function createGrid(rows, cols) {
-        for (let i = 0; i < rows * cols; i++) {
-            const square = document.createElement('div');
-            square.classList.add('square');
-            container.appendChild(square);
-        }
+function createGrid(rows, cols) {
+    for (let i = 0; i < rows * cols; i++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        container.appendChild(square);
     }
-   createGrid(16, 16);
+}
+ 
+function getRandomColor() {
+    let r = Math.floor(Math.random() *256)
+    let g = Math.floor(Math.random() *256)
+    let b = Math.floor(Math.random() *256)
+    return `rgb(${r},${g},${b})`
 
-   function handleHover(e) {
-    e.target.classList.add('active');
+}
+
+function handleHover(e) {
+    const randomColor = getRandomColor();
+    e.target.style.backgroundColor = randomColor;
 }
 
 function addHoverEffect() {
     const squares = document.querySelectorAll('.square');
     squares.forEach(square => {
         square.addEventListener('mouseover', handleHover);
-        square.style.backgroundColor = ''; // Set the initial background color to transparent
     });
 }
 
+createGrid(16, 16);
 addHoverEffect();
