91e762a71a4ff53ab36fc5f076b63c00db5f5232
AlinaDragu <alina_elena14@yahoo.com>
2024-01-17 19:46:36 +0200

    DOM

--- DOM/rock paper scissors/main.js
+++ DOM/rock paper scissors/main.js
@@ -1,34 +1,75 @@
-document.getElementById('rock').addEventListener('click', () => playGame('rock'));
-document.getElementById('paper').addEventListener('click', () => playGame('paper'));
-document.getElementById('scissors').addEventListener('click', () => playGame('scissors'));
+// document.getElementById('rock').addEventListener('click', () => playGame('rock'));
+// document.getElementById('paper').addEventListener('click', () => playGame('paper'));
+// document.getElementById('scissors').addEventListener('click', () => playGame('scissors'));
 
 
-const choices = ['rock', 'paper', 'scissors'];
+// const choices = ['rock', 'paper', 'scissors'];
+// let playerScore = 0;
+// let computerScore = 0;
+
+// function computerChoice() {
+//   const random = Math.floor(Math.random() * choices.length);
+//   return choices[random];
+// }
+
+// function theWinner(playerChoice, computerChoice) {
+//   if (playerChoice === computerChoice) {
+//     return 'It\'s a tie';
+//   } else if (
+//     (playerChoice === 'rock' && computerChoice === 'scissors') ||
+//     (playerChoice === 'paper' && computerChoice === 'rock') ||
+//     (playerChoice === 'scissors' && computerChoice === 'paper')
+//   ) { playerScore++;
+//     return 'You Won!';
+//   } else { computerScore++; 
+//     return 'You lost!';
+//   }
+// }
+
+// function playGame(playerChoice) {
+//   const compChoice = computerChoice();
+//   const result = theWinner(playerChoice, compChoice);
+//   document.getElementById('result').innerText = `You = ${playerChoice}. Computer = ${compChoice}. The result is that ${result}`;
+//   document.getElementById('score').innerText = `Score: Player ${playerScore} - Computer ${computerScore}`;
+// }
+
+
+
+
+document.querySelector('#rock').addEventListener('click', () => playGame('rock'))
+document.querySelector('#paper').addEventListener('click' , () => playGame('paper'))
+document.querySelector('#scissors').addEventListener('click' , () => playGame('scissors'))
+
+
+let choices = ['rock','paper','scissors']
 let playerScore = 0;
 let computerScore = 0;
 
-function computerChoice() {
-  const random = Math.floor(Math.random() * choices.length);
-  return choices[random];
+
+function computerChoice(){
+  const random = Math.floor(Math.random() * choices.length)
+  return choices[random]
 }
 
-function theWinner(playerChoice, computerChoice) {
-  if (playerChoice === computerChoice) {
-    return 'It\'s a tie';
-  } else if (
-    (playerChoice === 'rock' && computerChoice === 'scissors') ||
-    (playerChoice === 'paper' && computerChoice === 'rock') ||
-    (playerChoice === 'scissors' && computerChoice === 'paper')
-  ) { playerScore++;
-    return 'You Won!';
-  } else { computerScore++; 
-    return 'You lost!';
+
+function theWinner(playerChoice,computerChoice){
+  if(playerChoice===computerChoice){
+    return ' It\'s a tie'
+  }else if ((playerChoice==='rock' && computerChoice==='scissors') ||
+            (playerChoice==='paper' && computerChoice==='rock') ||
+            (playerChoice==='scissors' && computerChoice==='paper')){
+              playerScore++;
+              return 'You Won'
+    }else{
+      computerScore++;
+      return ' You lost'
+    }
   }
-}
+
 
 function playGame(playerChoice) {
   const compChoice = computerChoice();
-  const result = theWinner(playerChoice, compChoice);
-  document.getElementById('result').innerText = `You = ${playerChoice}. Computer = ${compChoice}. The result is that ${result}`;
-  document.getElementById('score').innerText = `Score: Player ${playerScore} - Computer ${computerScore}`;
+  const result = theWinner(playerChoice,compChoice);
+  document.querySelector('#result').innerText = `You chose ${playerChoice} . Computer chose ${compChoice}. And the result is ${result}`
+  document.querySelector('#score').innerText = `You re score is ${playerScore} - computer score is ${computerScore}`
 }
