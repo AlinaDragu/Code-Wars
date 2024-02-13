4427817c353d0713c016aa452c054a6aaf238a8a
AlinaDragu <alina_elena14@yahoo.com>
2024-02-13 17:41:16 +0200

    Array Methods

--- Array Methods/main.js
+++ Array Methods/main.js
@@ -43,3 +43,34 @@ let filtered = filterRange(arr, 1, 4);
 alert( filtered );
 
 alert( arr ); 
+
+
+//3.Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
+
+// The function should only modify the array. It should not return anything.
+
+// For instance:
+
+// let arr = [5, 3, 8, 1];
+
+// filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+
+// alert( arr ); // [3, 1]
+
+function filterRangeInPlace(arr2, a, b) {
+
+  for (let i = 0; i < arr2.length; i++) {
+    let val = arr2[i];
+    if (val < a || val > b) {
+      arr2.splice(i, 1);
+      i--;
+    }
+  }
+
+}
+
+let arr2 = [5, 3, 8, 1];
+
+filterRangeInPlace(arr2, 1, 4); 
+
+alert( arr2 ); 
