d77eae4f6fc54514befd10e8f855ab625b502967
AlinaDragu <alina_elena14@yahoo.com>
2024-02-15 18:57:22 +0200

    Array Methods

--- Array Methods/main.js
+++ Array Methods/main.js
@@ -168,3 +168,31 @@ let users = [ john, pete, mary ];
 let names = users.map(item => item.name)/* ... your code */
 
 alert( names ); // John, Pete, Mary
+
+//8.You have an array of user objects, each one has name, surname and id.
+
+// Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
+
+// For instance:
+
+let john2 = { name: "John", surname: "Smith", id: 1 };
+let pete2 = { name: "Pete", surname: "Hunt", id: 2 };
+let mary2 = { name: "Mary", surname: "Key", id: 3 };
+
+let users2 = [ john2, pete2, mary2 ];
+
+let usersMapped = users.map(user => ({
+  fullName: `${user.name} ${user.surname}`,
+  id: user.id
+})); /* ... your code ... */
+
+/*
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+*/
+
+alert( usersMapped[0].id ) // 1
+alert( usersMapped[0].fullName ) // John Smith
