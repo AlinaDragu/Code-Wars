4684890b2bb2d47336fcea7c57195500f6d5b97f
AlinaDragu <alina_elena14@yahoo.com>
2024-02-16 17:57:00 +0200

    Array Methods

--- Array Methods/main.js
+++ Array Methods/main.js
@@ -219,3 +219,25 @@ sortByAge(arr3);
 alert(arr5[0].name); // John
 alert(arr5[1].name); // Mary
 alert(arr5[2].name); // Pete
+
+
+//10.Write the function shuffle(array) that shuffles (randomly reorders) elements of the array.
+
+// Multiple runs of shuffle may lead to different orders of elements. For instance:
+
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+let arr6 = [1, 2, 3];
+
+shuffle(arr6);
+// arr = [3, 2, 1]
+
+shuffle(arr6);
+// arr = [2, 1, 3]
+
+shuffle(arr6);
+// arr = [3, 1, 2]
+// ...
+// All element orders should have an equal probability. For instance, [1,2,3] can be reordered as [1,2,3] or [1,3,2] or [3,1,2] etc, with equal probability of each case.
