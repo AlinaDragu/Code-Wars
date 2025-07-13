2a93f6557a45b83163f4d834b7fa7193ad3dc670
AlinaDragu <alina_elena14@yahoo.com>
2025-07-13 18:06:31 +0300

    8 kyu

--- 8 kyu Python/convert a number to a string.py
+++ 8 kyu Python/convert a number to a string.py
@@ -10,3 +10,5 @@
 def number_to_string(num):
      # Return a string of the number here!
     return str(num)
+
+print(repr(number_to_string(34))) #returns a string representation of an object that looks like you'd type it in code
