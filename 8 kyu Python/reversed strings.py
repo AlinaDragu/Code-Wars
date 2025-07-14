1559d778a780d66f15a142fcc86c5127f17ee8eb
AlinaDragu <alina_elena14@yahoo.com>
2025-07-14 19:26:23 +0300

    8 kyu

--- 8 kyu Python/reversed strings.py
+++ 8 kyu Python/reversed strings.py
@@ -4,6 +4,6 @@
 # 'word'   =>  'drow'
 
 def solution(string):
-    return string[::-1]
+    return string[::-1]  #take the entire string but iterate from the end to the beginning
 
 print(solution('classy'))
