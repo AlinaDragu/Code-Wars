fae7f1e6c2db65624af475e3c7cb81995ee5f594
AlinaDragu <alina_elena14@yahoo.com>
2025-07-24 11:29:06 +0300

    8 kyu

--- 8 kyu Python/find the smallest integer in the array.py
+++ 8 kyu Python/find the smallest integer in the array.py
@@ -5,3 +5,12 @@
 # Given [34, 15, 88, 2] your solution will return 2
 # Given [34, -345, -1, 100] your solution will return -345
 # You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+def find_smallest_int(arr):
+    smallest = arr[0]
+    for a in arr:
+        if a < smallest:
+            smallest = a  
+    return smallest  
+
+print(find_smallest_int([34,-2,-1,34,667,67]))
