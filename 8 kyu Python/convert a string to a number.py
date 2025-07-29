b77dbd731d71402086eabc4c4a45636796243beb
AlinaDragu <alina_elena14@yahoo.com>
2025-07-29 18:54:42 +0300

    8 kyu

---
+++ 8 kyu Python/convert a string to a number.py
@@ -0,0 +1,8 @@
+# We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+# Examples
+# "1234" --> 1234
+# "605"  --> 605
+# "1405" --> 1405
+# "-7" --> -7
+
