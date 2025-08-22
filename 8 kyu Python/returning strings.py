e66eaa476404878e1ec022a1f6d49b98c45d672f
AlinaDragu <alina_elena14@yahoo.com>
2025-08-22 11:32:18 +0300

    8 kyu

--- 8 kyu Python/returning strings.py
+++ 8 kyu Python/returning strings.py
@@ -1,2 +1,7 @@
 # Create a function that accepts a parameter representing a name and 
 # returns the message: "Hello, <name> how are you doing today?".
+
+def greet(name):
+    #Good Luck (like you need it)
+    return  f'Hello, {name} how are you doing today?'
+
