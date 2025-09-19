8528574bf17567ce630fc5c146474557f5d9a4b8
AlinaDragu <alina_elena14@yahoo.com>
2025-09-19 19:04:56 +0300

    8 kyu

--- 8 kyu Python/opposites attract.py
+++ 8 kyu Python/opposites attract.py
@@ -5,3 +5,7 @@
 
 def lovefunc( flower1, flower2 ):
     return (flower1 % 2 == 0 and flower2 % 2 != 0) or (flower1 % 2 != 0 and flower2 % 2 == 0)
+
+print(lovefunc(1,3))
+print(lovefunc(2,3))
+print(lovefunc(2,5))
